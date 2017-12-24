6b1f081062eba929581a265334309e573b7db9bb
onyxc <10016923+onyxc@users.noreply.github.com>
2017-12-23 21:30:42 -0600

    Update forms.py

--- django_remote_forms/forms.py
+++ django_remote_forms/forms.py
@@ -3,7 +3,6 @@ import six
 import warnings
 from django_remote_forms import fields, logger
 from django_remote_forms.utils import resolve_promise
-from django.utils.deprecation import RemovedInDjango19Warning
 
 class SortedDict(dict):
     """
